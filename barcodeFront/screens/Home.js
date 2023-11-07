12ab9f022c5db6ff95c9834cee77647d3e37a4ce
loginshin <95666408+loginshin@users.noreply.github.com>
2023-11-07 14:55:42 +0900

    infra) navigation

--- barcodeFront/screens/Home.js
+++ barcodeFront/screens/Home.js
@@ -1,23 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
-import TabNavigation from './navigation/TabNavigation';
+import React from 'react';
 
 
 export default function App() {
-  return (
-    <>
-        <TabNavigation />
-    </>
-  );
+return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+);
 }
 
 const styles = StyleSheet.create({
-  container: {
+container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+},
 });
