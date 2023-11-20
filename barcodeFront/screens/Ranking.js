84bdb8bc4a846f1b92ed6f4efe429de35b985f8b
changyunkim <102843599+changyunkim@users.noreply.github.com>
2023-11-20 16:25:44 +0900

    commit1)랭킹 기본모양

--- barcodeFront/screens/Ranking.js
+++ barcodeFront/screens/Ranking.js
@@ -1,22 +1,81 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
 import React from 'react';
 
-
 export default function Ranking() {
-return (
+  // Generate 10 items
+  const items = Array.from({ length: 10 }, (_, index) => (
+    <View key={index} style={styles.roundedRectangle}>
+      <View style={styles.imageContainer}>
+        <Image
+          source={require('../assets/icon.png')}
+          style={styles.imageStyle}
+        />
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.topText}>TOP{index + 1}</Text>
+        <Text style={styles.productName}>상품명</Text>
+        <Text style={styles.rating}>10/10</Text>
+      </View>
+    </View>
+  ));
+
+  return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        {items}
+      </ScrollView>
       <StatusBar style="auto" />
     </View>
-);
+  );
 }
-
 const styles = StyleSheet.create({
-container: {
+  container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-},
+  },
+  scrollContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  imageContainer: {
+    width: 80,
+    height: 80,
+    borderRadius: 10,
+    overflow: 'hidden',
+    marginLeft: 5,
+  },
+  roundedRectangle: {
+    flexDirection: 'row',
+    width: 400,
+    height: 100,
+    backgroundColor: '#F2F2F2',
+    borderRadius: 15,
+    margin: 10,
+    alignItems: 'center',
+  },
+  imageStyle: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 10,
+  },
+  textContainer: {
+    marginLeft: 10,
+  },
+  productName: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 20, // 여기에서 상품명과 평점 사이의 간격을 늘림
+  },
+  rating: {
+    fontSize: 14,
+    color: 'gray',
+  },
+  topText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 1, // Adjust the margin-bottom as needed
+  },
 });
