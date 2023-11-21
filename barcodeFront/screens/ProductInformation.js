ce42953236da19db532e85af6cfd92589763273b
loginshin <95666408+loginshin@users.noreply.github.com>
2023-11-22 00:21:17 +0900

    feat) 삼항연산자 적용, 크롤링 주석처리

--- barcodeFront/screens/ProductInformation.js
+++ barcodeFront/screens/ProductInformation.js
@@ -15,16 +15,14 @@ export default function ProductInformation({ route }) {
 
 
     const [imgUrl,setImgUrl]= useState({}); //상품 이미지 url
-    const [productData, setProductData]= useState({k:1}); //상품 정보
+    const [productData, setProductData]= useState({}); //상품 정보
     const [loading, setLoading]=useState(); //로딩 => 데이터 불러올때 사용
     const [productName,setProductName] = useState(); //상품 이름
-
+    const [productDivision, setProductDivision]=useState();
     
 
     const navigation = useNavigation();
     const {barcodeData} = route.params;
-
-
 
     useEffect(()=>{
         setImgUrl({url:"https://reactnative.dev/img/tiny_logo.png"}); //이미지 url 세팅
@@ -36,7 +34,10 @@ export default function ProductInformation({ route }) {
     const getData = async() =>{
         const response = await axios.get(`http://192.168.0.41:3000/barcodePage/?barcodeData=${barcodeData}`)
         .then(function (response) {
-            console.log(response.data);
+            console.log("요청 후 받아온 데이터",response.data);
+            setProductData(response.data)
+            setProductName(response.data[0].productNameKr);
+            setProductDivision(response.data[0].division);
         })
         .catch(function (error){
             console.log(error);
@@ -57,49 +58,70 @@ export default function ProductInformation({ route }) {
     // imageView
     
     //starView => starView 갯수에 따라 별 갯수 출력
-    function starView(){
-        var starView = 5; // 표시할 별 갯수
-        let stars = [];
+    // 평점 별 표시 기능
+    // function starView(){
+    //     var starView = 5; // 표시할 별 갯수
+    //     let stars = [];
         
-        for(let i = 0; i<starView; i++){
-            stars.push(<Image key={i} style={styles.starImg} resizeMode='contain' source={require('../assets/star.png')}/>);
-        }
-        return (
-            <View style={styles.starContainer}>
-                {stars}
-            </View>
-        )
-    }
+    //     for(let i = 0; i<starView; i++){
+    //         stars.push(<Image key={i} style={styles.starImg} resizeMode='contain' source={require('../assets/star.png')}/>);
+    //     }
+    //     return (
+    //         <View style={styles.starContainer}>
+    //             {stars}
+    //         </View>
+    //     )
+    // }
 
-
+sss
 
     return (
         <View style={styles.container}>
             <View style={styles.titleContainer}>
-                {titleName}
+                <Text style={{fontWeight:'bold', fontSize:width/18}}>{barcodeData}</Text>
             </View>
             <View style={styles.imageContainer}>
                 <Image style={styles.image} resizeMode='contain' source={require('../assets/imgless.jpeg')}/>
             </View>
             <View style={styles.topInfoContainer}>
                 {/* 별갯수 확인 */}
-                {starView()} 
+                {/* {starView()}  */}
                 <View style={styles.topInfo}>
                     <Text style={{fontWeight:'bold'}}>상품명</Text>
-                    <Text>{productName}</Text>
+                    {
+                        productName ?
+                        <Text>{productName}</Text> :
+                        <Text>상품을 찾지 못했습니다.</Text>
+                    }
+                    
                 </View>
                 <View style={styles.topInfo}>
                     <Text style={{fontWeight:'bold'}}>분류군</Text>
-                    <Text> 분류군입니다</Text>
+                    
+                    {
+                        productName ?
+                        <Text>{productDivision}</Text>:
+                        null
+                    }
                 </View>
             </View>
-            <Text>{barcodeData}</Text>
+            <View style={styles.bottomInfoContainer}>
+                <View style={styles.bottomInfoBox}>
+                    <Text style={{fontWeight:'bold'}}>상품설명</Text>
+                    <Text style={{marginLeft:width/5}}>gptText</Text>
+                </View>
+                <View style={styles.bottomInfoBox}>
+                    <Text style={{fontWeight:'bold'}}>구성성분</Text>
+                    <Text style={{marginLeft:width/5}}>구성성분text</Text>
+                </View>
+
+            </View>
+            {titleName}
+            
             <Button title="click get object start" onPress={handlePress}/>
         </View>
     );
     }
-
-
 
 
 const styles = StyleSheet.create({
@@ -134,9 +156,11 @@ const styles = StyleSheet.create({
     },
 
     topInfoContainer:{
-        height:height/10,
+        height:80,
+        width:width,
         backgroundColor:'#e4e4e4',
-        paddingLeft:40
+        paddingLeft:40,
+        paddingTop:height/60
     },
     
     starContainer:{
@@ -153,6 +177,19 @@ const styles = StyleSheet.create({
     topInfo:{
         flexDirection:'row',
         gap:width/100,
+        
+    },
+    bottomInfoContainer:{
+        flexDirection:'column',
+        width:width/1.2,
+        gap:50,
+        marginTop:30,
+        marginLeft:20,
+    },
+    bottomInfoBox:{
+        flexDirection:'column',
+        
+
         
     },
 });
--- nodeApi/routes/index.js
+++ nodeApi/routes/index.js
@@ -18,10 +18,11 @@ db.connect((err) => {
     if (err) throw err;
     console.log('Connected to the database');
 
-    const sql = 'SELECT * FROM products';
-    db.query(sql, (err, result) => {
+    const sql = 'show tables;';
+    db.query(sql,(err, result) => {
         if (err) throw err;
-        console.log(result);
+        // console.log(result);
+        console.log("connected!!!", result);
     })
 })
 
@@ -41,14 +42,49 @@ router.get('/barcodePage/', (req,res) => {
     const barcodeData = req.query.barcodeData;
     console.log('barcodePage 요청 data :', barcodeData )
     
+    // getData(barcodeData);
+    
     const sql = `select * from products where barcodeNum = '${barcodeData}'`;
     db.query(sql, (err, result) => {
         if (err) throw err;
         console.log(result);
         res.send(result);
     })
-    
 })
+
+
+
+//cheerio API 자바스크립트 크롤링 api↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+// function getData(barcodeNumber) {
+//     let url = `http://www.allproductkorea.or.kr/products/search?q=%7B%22mainKeyword%22:%22${barcodeNumber}%22,%22subKeyword%22:%22%22%7D&page=1&size=1`;
+//     let html = response.data;
+// let $ = cheerio.load(html);
+// $.html();
+// // 원하는 데이터를 가져옵니다.
+// let productCode = $('li[data-prd-no] em').text();
+// let productName = $('li[data-prd-no] strong').text();
+// let productCategory = $('li[data-prd-no] .spl_pm').text();
+// console.log(productCode, productName, productCategory);
+
+        
+//         if(response.status ===200){
+//             let html = response.data;
+//             let $ = cheerio.load(html);
+    
+//             //원하는 데이터를 가져옵니다.
+//             let productCode = $('li[data-prd-no] em').text();
+//             let productName = $('li[data-prd-no] strong').text();
+//             let productCategory = $('li[data-prd-no] .spl_pm').text();
+        
+//             console.log(productCode, productName, productCategory);
+//         }
+    
+// }
+
+
 
 module.exports = router;
 
