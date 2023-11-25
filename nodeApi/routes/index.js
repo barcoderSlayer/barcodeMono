b98be978f31ea891d8da397abe54a50a270d5c4a
changyunkim <102843599+changyunkim@users.noreply.github.com>
2023-11-25 21:57:46 +0900

    feat) db에서 상품명 바코드번호 바코드카운트 가져와서 top10 박스10개 생성

--- nodeApi/routes/index.js
+++ nodeApi/routes/index.js
@@ -7,10 +7,10 @@ const mysql = require('mysql');
 
 //db key config로 관리하기
 const db = mysql.createConnection({
-    host: 'localhost',
+    host: 'localhost',   
     user: 'root',
-    password: '4865',
-    database: 'barcoder'
+    password: '2848',
+    database: 'bacode'
 })
 
 //db 연결 확인
@@ -24,6 +24,10 @@ db.connect((err) => {
         console.log(result);
     })
 })
+
+// query 메서드를 Promise로 변환 
+const util = require('util');
+const queryAsync = util.promisify(db.query).bind(db);
 
 
 //router로 get 요청받기 test
@@ -41,7 +45,7 @@ router.get('/barcodePage/', (req,res) => {
     const barcodeData = req.query.barcodeData;
     console.log('barcodePage 요청 data :', barcodeData )
     
-    const sql = `select * from products where barcodeNum = '${barcodeData}'`;
+    const sql = `select * from products where barc odeNum = '${barcodeData}'`;
     db.query(sql, (err, result) => {
         if (err) throw err;
         console.log(result);
@@ -49,6 +53,18 @@ router.get('/barcodePage/', (req,res) => {
     })
     
 })
+
+router.get('/api/rankings', async (req, res) => {
+    try {
+        const sql = 'SELECT * FROM products ORDER BY scanCnt DESC LIMIT 10';
+        const rankings = await queryAsync(sql);
+        console.log('Rankings:', rankings); // 결과를 콘솔에 출력
+        res.json(rankings);
+    } catch (err) {
+        console.error('Error fetching rankings:', err);
+        res.status(500).send(err);
+    }
+});
 
 module.exports = router;
 
