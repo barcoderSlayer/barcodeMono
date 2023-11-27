a6cbfbae7f11958440f651b078a5d950f2f84b29
Unoren <95072336+Unoren@users.noreply.github.com>
2023-11-27 20:49:50 +0900

    DetailedCheck.js 수정
    오류난 formatTimestamp 주석 제거

--- DetailedCheck.js
+++ DetailedCheck.js
@@ -38,18 +38,6 @@ const DetailScreen = ({ navigation }) => {
     }).format(date);
   };
   
-//   const formatTimestamp = (timestamp) => {
-//     const date = new Date(timestamp);
-//     return new Intl.DateTimeFormat('ko-KR', {
-//       year: 'numeric',
-//       month: '2-digit',
-//       day: '2-digit',
-//       hour: '2-digit',
-//       minute: '2-digit',
-//       second: '2-digit',
-//     }).format(date);
-//   };
-
   return (
     <ScrollView style={styles.container}>
       <TouchableOpacity onPress={() => navigation.goBack()} style={styles.backButton}>
