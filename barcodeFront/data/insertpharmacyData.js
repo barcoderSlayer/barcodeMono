3812c210b267519bde29a0a368586f88c5661f27
parksh3070 <135408197+parksh3070@users.noreply.github.com>
2023-12-05 12:54:34 +0900

    data
    인서트문 병원이랑 약국으로 나누어서 작성하고 추가했습니다

---
+++ barcodeFront/data/insertpharmacyData.js
@@ -0,0 +1,33 @@
+const mariadb = require('mariadb');
+const fs = require('fs');
+
+const pool = mariadb.createPool({
+    host: '127.0.0.1', 
+    user: 'root', 
+    password: 'qkrtjdgh123!',
+    database: 'mydatabase'
+});
+
+async function insertData() {
+    let conn;
+    try {
+        // JSON 파일 읽기
+        const data = JSON.parse(fs.readFileSync('Pharmacyinfo.json', 'utf8'));
+        
+        conn = await pool.getConnection();
+        
+       // 데이터 삽입
+for (let item of data) {
+    await conn.query('INSERT INTO pharmacy (Name, PhoneNumber, Latitude, Longitude, Address, Codename) VALUES (?, ?, ?, ?, ?, ?)', [
+        item.요양기관명, item.전화번호, item.위도, item.경도, item.주소, item.종별코드명
+    ]);
+}
+
+    } catch (err) {
+        console.error(err);
+    } finally {
+        if (conn) await conn.end();
+    }
+}
+
+insertpharmacyData();
